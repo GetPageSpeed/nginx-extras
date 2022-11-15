72cfd4f300477d72d2d517a645d9eba0471575e6
Danila Vershinin <ciapnz@gmail.com>
2022-11-16 03:24:00 +0800

    Update discovery util scripts [skip ci]

--- util/discover.py
+++ util/discover.py
@@ -2,14 +2,15 @@
 import os
 import pickledb
 from github import Github
+from github.ContentFile import ContentFile
 from github.GithubException import UnknownObjectException, GithubException
 from github.Repository import Repository
 import logging as log
 
 log.basicConfig(level=log.INFO)
 g = Github(os.getenv("GITHUB_API_TOKEN"))
-db = pickledb.load('known.db', False)
-repositories = g.search_repositories(query='nginx in:name stars:>=1 archived:false', sort='stars')
+db = pickledb.load('known.db.json', False)
+repositories = g.search_repositories(query='nginx in:name stars:>=100 archived:false', sort='stars')
 
 repo: Repository
 i = 0
@@ -19,12 +20,18 @@ for repo in repositories:
     if known:
         log.info('Already known')
         continue
+    is_nginx_module = False
     try:
-        contents = repo.get_contents('config')
+        config_contents = repo.get_contents('config')
+        if isinstance(config_contents, ContentFile):
+            log.info('Config file detected in the root directory')
+            if 'ngx_addon_name' in config_contents.decoded_content.decode("utf-8"):
+                log.info('Config file has ngx_addon_name, so it is an NGINX module')
+                is_nginx_module = True
         log.info('NGINX module detected')
-        db.set(repo.full_name, {'module': True})
     except (UnknownObjectException, GithubException):
         log.warning(f"Has no config. Likely not a module")
-        db.set(repo.full_name, {'module': False})
+
+    db.set(repo.full_name, {'module': is_nginx_module})
     db.dump()
 
